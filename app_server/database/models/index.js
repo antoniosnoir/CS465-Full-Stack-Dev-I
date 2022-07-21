c3e2aed71f1ec3463a17fb8d34d1e62e6ebacd78
antoniosnoir <antonio.sobalvarroaguilera@snhu.edu>
2022-07-20 20:53:06 -0700

    Create object schema with Mongoose, populate MongoDB

--- app_server/database/models/index.js
+++ app_server/database/models/index.js
@@ -1,6 +1,6 @@
 var express = require('express'); //const
 var router = express.Router(); //var beofre 
-const ctrlMain = require('../controllers/main'); //const ctrlMain
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index); //ctrlMain.index
