1d3cf3e0f9bc8c96f1ef59fbd41c89649462d7fa
antoniosnoir <antonio.sobalvarroaguilera@snhu.edu>
2022-08-03 21:58:59 -0700

    Create new application API

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // allow CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'Get, POST, PUT, DELETE');
   next();
 });
