9536c7cfac2da19b6af50fd4af900c0f705626d5
antoniosnoir <antonio.sobalvarroaguilera@snhu.edu>
2022-08-05 21:45:42 -0700

    Create admin angular admin site

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { Trip } from "../models/trip";
+
+@Component({
+  selector: "app-trip-card",
+  templateUrl: "./trip-card.component.html",
+  styleUrls: ["./trip-card.component.css"],
+})
+export class TripCardComponent implements OnInit {
+  @Input("trip") trip: any;
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  private editTrip(trip: Trip): void {
+    localStorage.removeItem("tripCode");
+    localStorage.setItem("tripCode", trip.code);
+    this.router.navigate(["edit-trip"]);
+  }
+}
+
+/*import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
 
@@ -23,4 +45,4 @@ export class TripCardComponent implements OnInit {
     this.router.navigate(['edit-trip']);
   }
 
-}
+}*/
